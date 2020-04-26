d90af171560feae441beda2f511d37908ecd958d
Samuel Kim <m213474@usna.edu>
2020-04-26 15:51:42 -0400

    added a method to insert courses into db

--- courses/insertCourseCodes.php
+++ courses/insertCourseCodes.php
@@ -6,14 +6,14 @@
   if (mysqli_connect_errno()) {
     echo "<h5>ERROR: " . mysqli_connect_errno() . ": " . mysqli_connect_error() . " </h5><br>";
   }
-/*
-  function insertCourse($db) {
+
+  function insertCourse($db, $courseCode, $courseTitle) {
     $query = "INSERT INTO courses (courseCode, courseTitle)
                 VALUES (?, ?);";
 
     $stmt = $db->stmt_init();
     $stmt->prepare($query);
-    $stmt->bind_param('ss', ??);
+    $stmt->bind_param('ss', $courseCode, $courseTitle);
 
     $success = $stmt->execute();
     if (!$success || $db->affected_rows == 0) {
@@ -25,18 +25,21 @@
 
     return true;
   }
-*/
+
   //open filestream for list of courses
   $file = fopen("courses.csv","r");
+  //read in first line of format
+  fgets($file);
+
   //loop for reading file line by line
   while(!feof($file)) {
     $line = fgets($file);
     $courseArr = explode(',', $line);
-    print_r($courseArr);
+    //insert course into db
+    insertCourse($db, $courseArr[0], $courseArr[1]);
   }
 
   //close file stream
   fclose($file);
-
 ?>
 
