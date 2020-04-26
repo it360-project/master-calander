f7de747fd5fdb5c9487f836c68dc93c7dd2dc381
rococoscout <47163715+rococoscout@users.noreply.github.com>
2020-04-26 18:35:53 -0400

    Adds calander maker and course populater
    These are finished with comments, Enjoy!

---
+++ calander_retriever.php
@@ -0,0 +1,63 @@
+<?php
+  // Author: Jess Lonetti
+  // Title: Calander Retriever
+  // Date: 4.26.2020
+  // ****--------FUNCTIONALITY---------****
+  // Calander Retriever gathers an existing student
+  // and their courses to make a comprehessible calander
+  // that is accessible via javascript or direct placement.
+  // requirments: Student_Courses table entries, assignments
+  // table entries, User account.
+  //
+  //
+  require_once('mysql.inc.php');
+  include('calander_creator.php');
+  $db = new myConnectDB();
+  session_start();
+
+  $alpha=$_SESSION['user']['user'];
+
+
+  //****--------Start: Assignemnt data Retrieval---------****
+  // This segment retrieves the rows that are associated with the
+  // user account that has created a valid session.
+  $stmt = $db->stmt_init();
+  $query = "SELECT courseCode,assignmentDate,notes FROM assignments
+  WHERE courseCode IN(SELECT courseCode FROM student_courses WHERE alpha = ?);";
+  $stmt->prepare($query);
+  $stmt->bind_param('s',$alpha);
+  $check = $stmt->execute();
+  if (!$check || $db->affected_rows == 0)
+    echo "<h5>ERROR: " . $db->error . " for query *$query*assignmentinsert</h5><hr>";
+
+  $stmt->bind_result($courseCode,$assignmentDate,$notes);
+  $data = array(array());
+  while ($stmt->fetch()) {
+    $data[$assignmentDate][$courseCode]=json_decode($notes);
+  }
+  unset($data[0]);
+
+  $stmt->close();
+  //****--------End: Assignemnt data Retrieval---------****
+  //****--------Start: is it fall or spring?---------****
+  // Takes the current date and checks if the date is in the spring
+  // else it assumes the semester is in the fall.
+  $thisYear = date("Y");
+  $genDate = date("Y-m-d",strtotime($thisYear."-01-01"));
+  $date = date_create($genDate);
+
+  $startDate = date_format($date,"Y-m-d");
+  $genDate = date("Y-m-d",strtotime($thisYear."-05-29"));
+  $date = date_create($genDate);
+  $endDate = date_format($date,"Y-m-d");
+  $today = date("Y-m-d");
+  //****--------generateCal creates the calander based on time of year---------****
+  if(($today>=$startDate)&&($today<=$endDate))
+    generateCal($data,0,5);
+  else
+    generateCal($data,7,12);
+  //****--------End: is it fall or spring?---------****
+
+
+
+ ?>
---
+++ home.php
@@ -0,0 +1,114 @@
+<!-- visitors can only access this page if logged in -->
+  <!--
+  // Author: Jess Lonetti,Greg Polmatier,Sam Kim...CS Department(Navbar and CSS)
+  // Title: Master Calander
+  // Date: 4.26.2020
+  // ****--------FUNCTIONALITY---------****
+  // This allows access to a users whole schedule
+  // if calander is made using the default cs page style
+  //
+  </style>
+ -->
+<?php
+  // require_once('auth.inc.php');
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en" style="background-color:#F2FEFE;">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head;
+         any other head content must come *after* these tags -->
+
+    <!-- Icon to use on the browser bar -->
+    <link rel="icon" href="calendar/images/web-icon.png">
+
+    <!-- Bootstrap core CSS -->
+    <link href="calendar/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Skeleton CSS -->
+    <link rel="stylesheet" href="calendar/Skeleton/css/normalize.css">
+    <link rel="stylesheet" href="calendar/Skeleton/css/skeleton.css">
+    <link rel="stylesheet" href="calendar/css/skeleton-modifications.css">
+
+    <!-- Styles for the submission System -->
+    <link href="calendar/css/calendar-default.css" rel="stylesheet">
+
+
+    <!-- Highlight.js -->
+    <link rel="stylesheet" href="calendar/highlight/styles/color-brewer.css">
+    <script src='calendar/highlight/highlight.pack.js'></script>
+    <script>hljs.initHighlightingOnLoad();</script>
+
+    <!-- Font-Awesome -->
+    <link rel="stylesheet" type="text/css" href="calendar/Font-Awesome/css/font-awesome.min.css">
+
+    <!-- Datatables -->
+    <link rel="stylesheet" type="text/css" href="calendar/datatables.net/datatables.min.css"/>
+
+    <!-- Printing -->
+    <link rel="stylesheet" type="text/css" media="print" href="calendar/css/calendar-print.css" />
+
+    <!-- Custom CSS based on user preferences -->
+
+
+    <!-- Custom JavaScript based on user preferences -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="calendar/jquery/js/jquery-3.4.1.min.js"></script>
+    <script src="calendar/bootstrap/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <!-- <script src="calendar/bootstrap3-ie10-viewport-bug-workaround/ie10-viewport-bug-workaround.js"></script> -->
+    <!-- DataTables -->
+    <script type="text/javascript" src="calendar/datatables.net/datatables.min.js"></script>
+
+  <title>Master-Calander</title>
+
+  </head>
+  <body style="background-color:#F2FEFE;">
+
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <!--
+            <a class="navbar-brand" href="#">
+              <img alt="Navbar!" src="css/images/web-icon.png" width="24">
+            </a>
+          -->
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+
+            <li><a href="#">
+                Master Calander</a></li>
+            <li><a href="home.php?logoff=1">Sign out</a></li>
+            <li><a href="http://submit.cs.usna.edu/site/home.php">Submission</a></li>
+
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+  <!-- End TopBar and CSS Stuff! -->
+<!-- Begin providing the contents of the page -->
+<div class="container" id="cal">
+
+
+</div>
+ <!-- /container AKA CALANDER -->
+</body>
+<script src="calander.js"></script>
+</html>
